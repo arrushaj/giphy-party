c9dbb96ad4b8664d8504068313a64030322e49a5
Ardit Rrushaj <arditrrushaj@gmail.com>
2022-11-15 17:57:02 -0600

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+"use strict";
+
 console.log("Let's get this party started!");
+
+console.log('s');
+
+const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+
+let form = $('form').on('submit', searchGiphy);
+
+console.log(form);
+
+async function searchGiphy(evt) {
+  evt.preventDefault();
+  let search = $('#term').val();
+  console.log(search);
+
+  let response = await axios.get("http://api.giphy.com/v1/gifs/search",
+    {params: {q: search, api_key: API_KEY}});
+
+  console.log("Response=", response);
+}
