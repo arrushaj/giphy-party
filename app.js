176c11b0c3da0de51c053b10bf6e242562f0a178
David Luo <davidkluo@gmail.com>
2022-11-15 16:15:50 -0800

    Implement add and remove gifs

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 "use strict";
 
-console.log("Let's get this party started!");
-
-console.log('s');
-
 const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
 
-let form = $('form').on('submit', searchGiphy);
+$('form').on('submit', searchGiphy);
+$('#remove').on('click', removeGifs);
 
-console.log(form);
+function removeGifs(evt) {
+  $('#gifs').empty();
+}
+
+function addGif(gifUrl) {
+  const $newGif = $('<img>').attr('src', gifUrl);
+  $('#gifs').append($newGif);
+}
 
 async function searchGiphy(evt) {
   evt.preventDefault();
-  let search = $('#term').val();
-  console.log(search);
 
-  let response = await axios.get("http://api.giphy.com/v1/gifs/search",
+  const search = $('#term').val();
+  const response = await axios.get("http://api.giphy.com/v1/gifs/search",
     {params: {q: search, api_key: API_KEY}});
 
-  console.log("Response=", response);
+  const gifUrl = response.data.data[0].images.original.url;
+  addGif(gifUrl);
 }
